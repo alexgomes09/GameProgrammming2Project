aba94ac9ee71c5cbfe486516ca1a927083c0f1a1
alexgomes09 <alex_09hg@yahoo.com>
2014-11-21 22:18:02 -0500

    Cloud is working as expected

--- Assets/Scripts/Cloud_Swan.cs
+++ Assets/Scripts/Cloud_Swan.cs
@@ -1,24 +1,16 @@
-﻿using UnityEditor;
-using UnityEngine;
-using System.Collections;
+﻿using UnityEngine;
 
 public class Cloud_Swan : MonoBehaviour
 {
     private GameObject player;
     public GameObject swan,cloud;
-    public float cloudSpeed = .5f;
+    private const float cloudSpeed = 1.2f;
 
-	void Start ()
+    void Start ()
 	{
 	    player = GameObject.Find("Player");
         swan = GameObject.Find("swan");
-
-	    if (cloud == null)
-	    {
-            cloud = Instantiate(Resources.Load<GameObject>("cloud"),new Vector2(player.transform.position.x+14,player.transform.position.y+5)
-                ,Quaternion.identity) as GameObject;
-	    }
-        
+	    cloud= GameObject.Find("cloud");
 	}
 
     private void Update()
@@ -28,21 +20,16 @@ public class Cloud_Swan : MonoBehaviour
 
     void MoveCloud()
     {
-        if (cloud != null)
-        {
-            cloud.transform.Translate(-Vector2.right * cloudSpeed);
-        }
+        cloud.transform.Translate(-Vector2.right * Time.deltaTime*cloudSpeed);
     }
 
     void OnBecameInvisible()
     {
-        DestroyImmediate(cloud.gameObject, true);
-        if (cloud == null)
+        if (cloud.transform.position.x < 0)
         {
-            Instantiate(Resources.Load<GameObject>("cloud"), new Vector2(player.transform.position.x + 14, player.transform.position.y + 5), Quaternion.identity);
+            cloud.transform.position = new Vector2(player.transform.position.x+14,player.transform.position.y+5);
+            Debug.Log(Camera.main.aspect);
+            Debug.Log(Screen.height);
         }
-        
     }
-
-    
 }
