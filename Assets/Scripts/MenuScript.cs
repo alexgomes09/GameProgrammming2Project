e4013ae72f0e1d9d038956f7fe5e976b60a01dbf
Alex.Liu <alexliubo@gmail.com>
2014-11-24 20:59:24 -0500

    add about and how to menu and scenes

--- Assets/Scripts/MenuScript.cs
+++ Assets/Scripts/MenuScript.cs
@@ -15,6 +15,20 @@ public class MenuScript : MonoBehaviour
 		// Center in X, 2/3 of the height in Y
 		Rect buttonRect = new Rect(
 			Screen.width / 2 - (buttonWidth / 2),
+			(1 * Screen.height / 3) - (buttonHeight / 2),
+			buttonWidth,
+			buttonHeight
+			);
+
+		Rect buttonRectAbout = new Rect(
+			Screen.width / 2 - (buttonWidth / 2),
+			(1.5f * Screen.height / 3) - (buttonHeight / 2),
+			buttonWidth,
+			buttonHeight
+			);
+
+		Rect buttonHow = new Rect(
+			Screen.width / 2 - (buttonWidth / 2),
 			(2 * Screen.height / 3) - (buttonHeight / 2),
 			buttonWidth,
 			buttonHeight
@@ -26,6 +40,20 @@ public class MenuScript : MonoBehaviour
 			// On Click, load the first level.
 			// "Stage1" is the name of the first scene we created.
 			Application.LoadLevel("Level1");
+		}
+
+		if(GUI.Button(buttonRectAbout,"About!"))
+		{
+			// On Click, load the first level.
+			// "Stage1" is the name of the first scene we created.
+			Application.LoadLevel("about");
+		}
+
+		if(GUI.Button(buttonHow,"How to Play!"))
+		{
+			// On Click, load the first level.
+			// "Stage1" is the name of the first scene we created.
+			Application.LoadLevel("howto");
 		}
 	}
 }
