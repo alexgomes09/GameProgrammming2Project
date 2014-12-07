eddec51e72dae0889315e19b9bde0e83c804a4bf
alexgomes09 <alex_09hg@yahoo.com>
2014-12-06 21:53:12 -0500

    Added a fox but some animaion bug

---
+++ Assets/Scripts/fox.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class fox : MonoBehaviour
+{
+
+    private GameObject player;
+    private float foxSpeed = 0.5f;
+	void Start ()
+	{
+	    player = GameObject.Find("Player");
+	}
+	
+	void Update ()
+	{
+	    gameObject.transform.position = -Vector2.right*foxSpeed*Time.deltaTime;
+	}
+}
